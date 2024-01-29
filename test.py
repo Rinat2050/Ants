e41131c121ce63d64f94fdb5bbb039ccb8705fab
Rinat2050 <sssr2050@yandex.ru>
2024-01-29 08:57:10 +0300

    Новый майн

--- test.py
+++ test.py
@@ -1,63 +1,30 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QGridLayout
-from PyQt5.QtGui import QPainter, QColor, QBrush, QPixmap, QPalette, QPen, QColor
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
-HEX_COORD_X = 50
-HEX_COORD_Y = 50
-HEX_WIDTH = 100
-HEX_HEIGHT = int(HEX_WIDTH * (3 ** 0.5 / 2))
 
 class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.initUI()
-        #self.initUI()й
-    def initUI(self):
-        self.setWindowTitle("Ants")  # заголовок окна
-        self.move(1000, 100)  # положение окна
-        self.resize(800, 800)  # размер окна
-
-    def keyPressEvent(self, event):
-        if event.key() == QtCore.Qt.Key_A:
-            ant2 = Ant(index_to_coord(4, 4))
-            ant.move(5, 5)
-            print("Press a")
-        event.accept()
-
-
-
-class Ant(QWidget):
     def __init__(self):
         super().__init__()
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_D:
-            # ant2 = Ant(index_to_coord(4, 4))
-            # ant.move(55, 55)
-            print("Press d")
-            #self.show()
-        #event.accept()
+        self.setWindowTitle("My App")
+        self.label = QLabel("Click in this window")
+        self.setCentralWidget(self.label)
+        button = QPushButton("Press Me!")
+        button.clicked.connect(self.the_button_was_clicked)
 
 
+    def the_button_was_clicked(self):
+        print("Clicked!")
 
+    def mouseMoveEvent(self, e):
+        self.label.setText("mouseMoveEvent")
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    ant = Ant()
-    ant.show()
-    #app.installEventFilter(ant)
-    win.show()
-    sys.exit(app.exec_())
+    def mousePressEvent(self, e):
+        self.label.setText("mousePressEvent")
 
+app = QApplication(sys.argv)
 
+window = MainWindow()
+window.show()
 
-
-
-
-
-
-
-
+app.exec()
