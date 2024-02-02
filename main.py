0ceac7a41f156511b9ccb2309fdc287f317949af
Rinat2050 <sssr2050@yandex.ru>
2024-02-02 11:05:00 +0300

    Чё-то делали

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import sys
 import math
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QGridLayout
 from PyQt5.QtGui import QPainter, QColor, QBrush, QPixmap, QPalette, QPen, QColor
 from PyQt5.QtCore import Qt, QSize, QPoint
 
-
-HEX_COORD_X = 50
-HEX_COORD_Y = 50
-HEX_WIDTH = 100
-HEX_HEIGHT = int(HEX_WIDTH * (3 ** 0.5 / 2))
+HEX_FIELD_X0 = 150
+HEX_FIELD_Y0 = 150
+# HEX_WIDTH = 100
+# HEX_HEIGHT = int(HEX_WIDTH * (3 ** 0.5 / 2))
 HEX_LENGTH = 50
 
 
-class MyWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.initUI()   # параметры окна в отдельной функции
+        self.initUI()  # параметры окна в отдельной функции
+        layout = QVBoxLayout()  # Создаем вертикальный макет
+        hex_field = HexField()
+        layout.addWidget(hex_field)  # Добавляем "поле гексов" в макет
+        self.setLayout(layout)  # Устанавливаем макет для главного окна
 
     def initUI(self):
         self.setWindowTitle("Ants")  # заголовок окна
         self.move(1000, 100)  # положение окна
         self.resize(800, 800)  # размер окна
+
+
+class HexField(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()  # Создаем вертикальный макет
+        for i in range(5):
+            hex = Hex(10, 10)
+            hex.move(i*100, 50)
+            layout.addWidget(hex)
+        self.setLayout(layout)  # Устанавливаем макет для виджета
+
+
 class Hex(QWidget):
     def __init__(self, x, y):
         super().__init__()
@@ -30,40 +46,29 @@ class Hex(QWidget):
 
     def paintEvent(self, event):
         self.painter = QPainter(self)
+        self.painter.setRenderHint(QPainter.Antialiasing)  # сглаживание углов
         self.painter.setPen(QPen(Qt.green, 5, Qt.SolidLine))
-        self.painter.drawEllipse(self.x, self.y, HEX_WIDTH, HEX_HEIGHT)
-        # self.painter.drawPolygon([
-        #                         QPoint(self.x - HEX_LENGTH // 2, self.y - int(HEX_LENGTH * (3 ** 0.5 / 2))),
-        #                         QPoint(self.x + HEX_LENGTH // 2, self.y - int(HEX_LENGTH * (3 ** 0.5 / 2))),
-        #                         QPoint(self.x + HEX_LENGTH // 2, self.y - int(HEX_LENGTH * (3 ** 0.5 / 2))),
-        #                         QPoint(self.x + HEX_LENGTH, self.y + int(HEX_LENGTH * (3 ** 0.5 / 2)))
-        #           #                         ])
-        self.painter.drawPolygon(hexagon_coordinates(HEX_LENGTH, self.x, self.y))
+        # self.painter.drawEllipse(self.x, self.y, 50, 100)
+        self.painter.drawPolygon(hex_painting(self.x, self.y))
 
 
-def hexagon_coordinates(s, center_x, center_y):
+def hex_painting(center_x, center_y):
     coordinates = []
     for i in range(6):
-        x = int(center_x + s * math.cos(i * 2 * math.pi / 6))
-        y = int(center_y + s * math.sin(i * 2 * math.pi / 6))
+        x = int(center_x + HEX_LENGTH * math.cos(i * 2 * math.pi / 6))
+        y = int(center_y + HEX_LENGTH * math.sin(i * 2 * math.pi / 6))
         coordinates.append(QPoint(x, y))
     return coordinates
 
 
-
+def index_in_coord(i, j):
+    x = HEX_FIELD_X0 + i * math.cos(j * 2 * math.pi / 6)
+    y = HEX_FIELD_Y0 + j * math.sin(j * 2 * math.pi / 6)
+    return x, y
 
 
 if __name__ == "__main__":
-    print(hexagon_coordinates(100, 0, 0))
     app = QApplication(sys.argv)
-    win = MyWindow()
-
-    hex = Hex(200, 300)
-    win.setCentralWidget(hex)
-
-    #hex_field = Hex_field(3, 4)
-
-
-    win.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
-
--- test.py
+++ test.py
@@ -1,30 +1,156 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+## ООП автосалон
+# TODO: обозначения координат на доске chess_field = Frame()
+# TODO: возможность отключения подцветки для коня Checkbutton
+# TODO: убрать зелёные после победы
+
+from tkinter import *
+from pprint import pprint
+from tkinter.messagebox import askquestion
+from tkinter import ttk
+
+# tkinter обрабатывает события
+
+count = 0  # глобальная переменная для счетчика ходов
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def click(M, row, column):  # функция клика по кнопке
+    global count  # делаем переменную видимой для функции click
+    if valid_step(M, row, column):
+        x, y = search_K(M)
+        M[x][y] = ' '
+        M[row][column] = 'K'
+        count += 1
+        update_window(M)
+        color_valid_step(M, list_of_button)
+        lbl_count.config(text=f'Ходов: {count}')
+        if check_win(M):
+            askquestion(title="УРА !!!", message=f'Победа! \nВы победили за {count} ходов')
+            M[0][0] = 'K'
+            M[7][7] = ' '
+            count = 0
+            lbl_count.config(text=f'Ходов: {count}')
+            update_window(M)
+            color_valid_step(M, list_of_button)
+    else:
+        lbl_error_step.grid_forget()
+        lbl_error_step.grid(row=2, column=8)
 
-        self.setWindowTitle("My App")
-        self.label = QLabel("Click in this window")
-        self.setCentralWidget(self.label)
-        button = QPushButton("Press Me!")
-        button.clicked.connect(self.the_button_was_clicked)
+
+def color_valid_step(M, list):  # функция окрашивания кнопок для возможного хода
+    # print(enabled.get())
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            if valid_step(M, i, j):
+                list_in_matrix(list)[i][j].configure(bg='green')
+            else:
+                if (i + j) % 2 == 0:
+                    list_in_matrix(list)[i][j].configure(bg='white')
+                else:
+                    list_in_matrix(list)[i][j].configure(bg='gray')
 
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
+def update_window(M):  # функция обновления окна
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            list_in_matrix(list_of_button)[i][j].configure(text=M[i][j])
 
-    def mouseMoveEvent(self, e):
-        self.label.setText("mouseMoveEvent")
 
-    def mousePressEvent(self, e):
-        self.label.setText("mousePressEvent")
+def search_K(M):  # функция ищет и возвращает координаты К
+    for row in range(len(M)):
+        for column in range(len(M[row])):
+            if M[row][column] == 'K':
+                return row, column
 
-app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+def list_in_matrix(list):  # функция перобразует список в матрицу
+    res = []
+    sub = []
+    for i in range(len(list)):
+        sub.append(list[i])
+        if (i + 1) % 8 == 0:
+            res.append(sub)
+            sub = []
+    return res
 
-app.exec()
+
+def valid_step(M, x2, y2):  # функция проверяет возможность хода
+    x1, y1 = search_K(M)
+    if abs(x2 - x1) == 1 and abs(y2 - y1) == 2 or abs(x2 - x1) == 2 and abs(y2 - y1) == 1:
+        return True
+    else:
+        return False
+
+
+def check_win(M):  # функция проверяет победу
+    if search_K(M) == (7, 7):
+        return True
+    else:
+        return False
+
+
+def checkbutton_changed():
+    pass
+    # print(enabled.get())
+
+
+window = Tk()
+window.title("Конь")
+window.geometry('530x500')
+
+M = []
+for _ in range(8):
+    sub_M = []
+    for j in range(8):
+        sub_M.append(' ')
+    M.append(sub_M)
+
+M[0][0] = 'K'
+# pprint(M)
+
+list_of_button = []  # список кнопок
+chess_field = Frame(window)  # метод создания рамки
+chess_field.grid(row=2, column=0)
+
+param_field = Frame(window)
+param_field.grid(row=2, column=3)
+
+x1_field = Frame(window)
+x1_field.grid(row=0, column=0)
+alfa = 'abcdefgh'
+for i in range(8):
+    lable_x1 = Label(x1_field, text=alfa[i], width=6, height=1)
+    lable_x1.grid(row=0, column=i)
+    lable_x1.grid(row=3, column=i)
+
+y1_field = Frame()
+y1_field.grid(row=2, column=0)
+
+for row in range(8):
+    for column in range(8):
+        if (row + column) % 2 == 0:
+            color = 'white'
+        else:
+            color = 'gray'
+        btn = Button(chess_field, text=(M[row][column]),
+                     width=3, height=3, bg=color,
+                     command=lambda M=M, row=row, column=column: click(M, row,
+                                                                       column))  # lambda - анонимная функция что передает лямбда в функцию click
+        btn.grid(row=row, column=column)  # размещение кнопки в окне
+        list_of_button.append(btn)
+color_valid_step(M, list_of_button)
+
+# создание счетчика ходов
+lbl_count = Label(param_field, text=f'Ходов: {count}')
+lbl_count.config(width=10, height=1)
+lbl_count.grid(row=0, column=8)
+
+# создание кнопки для включения подсветки
+enabled = IntVar()
+checkbutton = ttk.Checkbutton(param_field, text="Подсветка", variable=enabled, command=checkbutton_changed)
+checkbutton.grid(row=1, column=8)
+
+# вывод текста "Неверный ход"
+lbl_error_step = Label(param_field, text='Неверный\nход', fg='red')
+lbl_error_step.config(width=10, height=2)
+
+window.mainloop()
