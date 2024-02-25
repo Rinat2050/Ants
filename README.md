07b1130eb55f3b69b46dd85884a3c9d7f293cf69
KanVull <minirorus2@gmail.com>
2024-02-25 18:28:30 +0400

    Create environment, Add README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ants
+
+Study project
+
+## Building
+
+Create environment (Windows)
+```
+virtualenv .venv
+cd .venv/Scripts
+activate
+pip install -r ../../requirements.txt
+```
+
+## Start the project
+
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import Tk, Canvas
+from ant import Ant
+from hex import Hex
+from constants import *
+from calculate import index_to_coord
+
+
+window = Tk()
+window.title('ANTS')
+window.geometry(str(WIDTH_WINDOW) + 'x' + str(HEIGHT_WINDOW) + '+1100+0')
+
+
+class Place(Canvas):
+    def __init__(self, root):
+        super().__init__(root, width=WIDTH_WINDOW, height=HEIGHT_WINDOW)
+        self.grid(row=0, column=0)
+
+
+
+place_hex = Place(window)
+
+for i in range(12):
+    for j in range(12):
+        if (index_to_coord(i - 6, j)[0])**2 + (index_to_coord(i, j - 6)[1])**2 <= 350**2:
+            b = Hex(i, j, place_hex)
+
+ant = Ant(6, 11, place_hex)
+
+
+window.mainloop()
