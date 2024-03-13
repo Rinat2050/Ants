74ad6eb5ee3d4ee42aeae928e959d9fa00d2ecbf
kanvull <minirorus2@gmail.com>
2024-03-13 18:28:30 +0400

    Fix hardcoded window pos. Add relative position

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from tkinter import Tk
 import constants
 from place import Place
 
+
 window = Tk()
 window.title('ANTS')
-window.geometry('{w}x{h}+1100+0'.format(
-    w=constants.WIDTH_WINDOW,
-    h=constants.HEIGHT_WINDOW),
-)
 
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x_offset = (screen_width - constants.WIDTH_WINDOW) // 2
+y_offset = (screen_height - constants.HEIGHT_WINDOW) // 2
+
+window.geometry('{w}x{h}+{x}+{y}'.format(
+    w=constants.WIDTH_WINDOW,
+    h=constants.HEIGHT_WINDOW,
+    x=x_offset,
+    y=y_offset,
+))
 
 place_hex = Place(window)
 
